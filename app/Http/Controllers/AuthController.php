e7e453ab3bf648331b16e96f7b3cf3680174706f
burritos <sibobbi101@gmail.com>
2024-11-16 04:14:11 +0700

    feat: добавил авторизацию

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Users\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request): \Illuminate\Http\JsonResponse|array
+    {
+        $credentials = $request->validated();
+        $user = User::query()->where('email', $credentials['email'])->first();
+
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $request->user()->createToken('api');
+
+        return ['token' => $token->plainTextToken];
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->status(200);
+    }
+}
